0350d7d05244b85b5167d01d48b338642023fe5f
sameendraK <sameendrak@gmail.com>
2024-01-11 21:23:28 +0530

    feat(app.component): Store data in local storage using reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+interface DataObj {
+  email: string,
+  phone: string,
+  hexCode: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,4 +15,20 @@ export class AppComponent {
   title = 'Unique Schools';
 
   numberOfJoinees: number = 0;
+
+  data = { id: 1, name: "Sameendra", code: "+91" }
+  userForm: FormGroup = new FormGroup(
+    {
+      email: new FormControl(),
+      phone: new FormControl(),
+      hexCode: new FormControl()
+    }
+  )
+
+  localStorageArray: DataObj[] = []
+
+  saveData() {
+    this.localStorageArray.push(this.userForm.value)
+    localStorage.setItem('tableData', JSON.stringify(this.localStorageArray))
+  }
 }
