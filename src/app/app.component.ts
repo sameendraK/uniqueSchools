3db7a177a1cf9fe41f36f1c85327bf99625f2b3b
sameendraK <sameendrak@gmail.com>
2024-01-11 20:48:53 +0530

    feat(app.component, styles.scss): Add new styling and global variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'uniqueSchools';
+  title = 'Unique Schools';
+
   numberOfJoinees: number = 0;
 }
